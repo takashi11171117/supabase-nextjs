41fe97f653235833f61ba6cc58786ad0c004fba0
takashi11171117 <takashi11171117@gmail.com>
2023-01-27 19:13:11 +0900

    add navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,9 @@ import { useState } from "react";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider, Session } from "@supabase/auth-helpers-react";
 import { AppProps } from "next/app";
+import { NextUIProvider } from "@nextui-org/react";
+import { Box } from "@/components/Box";
+import Navbar from "@/components/NavbarComponent";
 
 function MyApp({
   Component,
@@ -17,7 +20,21 @@ function MyApp({
       supabaseClient={supabase}
       initialSession={pageProps.initialSession}
     >
-      <Component {...pageProps} />
+      <NextUIProvider>
+        <Navbar />
+        <Box
+          css={{
+            px: "$12",
+            py: "$15",
+            mt: "$12",
+            "@xsMax": { px: "$10" },
+            maxWidth: "800px",
+            margin: "0 auto",
+          }}
+        >
+          <Component {...pageProps} />
+        </Box>
+      </NextUIProvider>
     </SessionContextProvider>
   );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,31 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { CssBaseline } from "@nextui-org/react";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+import React from "react";
 
-export default function Document() {
-  return (
+class MyDocument extends Document {
+  static async loadGetInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: React.Children.toArray([initialProps.styles]),
+    };
+  }
+
+  return() {
     <Html lang="en">
-      <Head />
+      <Head>{CssBaseline.flush()}</Head>
       <body>
         <Main />
         <NextScript />
       </body>
-    </Html>
-  )
+    </Html>;
+  }
 }
+
+export default MyDocument;
